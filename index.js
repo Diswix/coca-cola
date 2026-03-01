e17d058238e020a2863465ddf45b9efa91801baf
Diswix <coololeksandra7@gmail.com>
2026-03-01 16:51:14 +0200

    sss

--- index.js
+++ index.js
@@ -8,7 +8,33 @@ const server = http.createServer((req, res) => {
     if (req.url == '/jokes' && req.method == 'GET') {
         getAllJokes(res, req);
     }
+
+    if (req.url == '/jokes' && req.method == 'POST') {
+        addJokes(res, req);
+    }
 });
 
 server.listen(3000);
+
+function getAllJokes(res, req) {
+
+    let dir = fs.readdirSync(dataPath);
+    let allJokes = [];
+
+    for (let i = 0; i < dir.length; i++) {
+        let file = fs.readFileSync(
+            path.join(dataPath, i + '.js')
+        );
+
+        let jokeJson = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJson);
+        
+        joke.id = i;
+
+        allJokes.push(joke);
+    }
+
+    res.end(JSON.stringify(allJokes));
+}
+
 

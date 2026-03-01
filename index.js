4c769277e6629f675f7ef378971d81307fb5dcab
Diswix <coololeksandra7@gmail.com>
2026-03-01 16:41:40 +0200

    kkk

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, 'data');
 
 const server = http.createServer((req, res) => {
-    if(req.url == '/jokes' && req.method == 'GET') {
-        getAllJokes(res,req);
-}});
+    if (req.url == '/jokes' && req.method == 'GET') {
+        getAllJokes(res, req);
+    }
+});
+
 server.listen(3000);
+

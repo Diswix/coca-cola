fab17f3c6bf72737cd5fc1ac2c6c6a4b5fc74478
Diswix <coololeksandra7@gmail.com>
2026-03-08 17:32:52 +0200

    ll

--- data/0.js
+++ data/0.js
@@ -1,5 +1,3 @@
 {
-    "content": "why Shevchenko never went to the jym? because he had kripatyra",
-    "likes": 0,
-    "dislikes": 0
+    "content": "why Shevchenko never went to the jym? because he had kripatyra", "likes": 0, "dislikes": 0
 }

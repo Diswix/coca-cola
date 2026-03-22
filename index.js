0ff035b490bfa2c5f1943a123d76d3273afee231
Diswix <coololeksandra7@gmail.com>
2026-03-22 17:34:10 +0200

    ss

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function getAllJokes(res, req) {
 
     for (let i = 0; i < dir.length; i++) {
         let file = fs.readFileSync(
-            path.join(dataPath, i + '.js')
+            path.join(dataPath, i + '.json')
         );
 
         let jokeJson = Buffer.from(file).toString();
@@ -45,6 +45,8 @@ function getAllJokes(res, req) {
 
         allJokes.push(joke);
     }
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     res.end(JSON.stringify(allJokes));
 };
@@ -62,7 +64,7 @@ function addJokes(res, req) {
         joke.dislikes = 0;
 
         let files = fs.readdirSync(dataPath);
-        let fileName = dir.length + '.js';
+        let fileName = dir.length + '.json';
         fs.writeFileSync = path.join(dataPath, fileName);
         fs.writeFileSync(dataPath, JSON.stringify(joke));
 
@@ -74,7 +76,7 @@ function like(res, req) {
     const params = url.parse(req.url, true).query;
     let id = params.id;
     if (id) {
-        let filePath = path.join(dataPath, id + '.js');
+        let filePath = path.join(dataPath, id + '.json');
         let file = fs.readFileSync(filePath);
         let jokeJson = Buffer.from(file).toString();
         let joke = JSON.parse(jokeJson);
@@ -88,7 +90,7 @@ function dislike(res, req) {
     const params = url.parse(req.url, true).query;
     let id = params.id;
     if (id) {
-        let filePath = path.join(dataPath, id + '.js');
+        let filePath = path.join(dataPath, id + '.json');
         let file = fs.readFileSync(filePath);
         let jokeJson = Buffer.from(file).toString();
         let joke = JSON.parse(jokeJson);
